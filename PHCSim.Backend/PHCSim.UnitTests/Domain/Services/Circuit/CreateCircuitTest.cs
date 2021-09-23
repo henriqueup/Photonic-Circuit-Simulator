165d12c9dc2fb912992d4d2e1142fe074a47aabd
henriqueup <titepessoa@gmail.com>
2021-09-22 21:27:50 -0300

    Created the first unit test

---
+++ PHCSim.Backend/PHCSim.UnitTests/Domain/Services/Circuit/CreateCircuitTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PHCSim.Domain.Repositories;
+using PHCSim.Domain.Services;
+
+namespace PHCSim.UnitTests.Domain.Services.Circuit
+{
+    [TestClass]
+    public class CreateCircuitTest
+    {
+        private readonly Mock<ICircuitRepository> circuitRepository = new Mock<ICircuitRepository>();
+
+        private CircuitService circuitService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            circuitService = new CircuitService(circuitRepository.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            circuitRepository.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void CreateCircuit_Success()
+        {
+            circuitService.Create("test 1");
+
+            circuitRepository.Verify(m => m.Create("test 1"), Times.Once);
+        }
+    }
+}
